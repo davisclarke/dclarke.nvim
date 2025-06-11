a87a185c478f25e06afbb596aab98d4a59e204d1
davisclarke <clarked126@gmail.com>
2025-06-11 03:28:24 -0500

    made agenda more smooth

--- init.lua
+++ init.lua
@@ -129,7 +129,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
+vim.api.nvim_create_user_command('GitAutoCommitPush', function()
+  local date = os.date '%Y-%m-%d %H:%M:%S'
+  local commit_cmd = string.format("git add . && git commit -m ': `date`' && git push", date)
+  local output = vim.fn.system(commit_cmd)
+  if vim.v.shell_error == 0 then
+    vim.notify('Git commit/push 󰄬 ' .. date, vim.log.levels.INFO)
+  else
+    vim.notify('Git commit/push failed: ' .. output, vim.log.levels.ERROR)
+  end
+end, {})
 -- Auto-update markdown
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'markdown' },
@@ -137,9 +146,10 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.schedule(function()
       vim.keymap.set(
         'n',
-        '<leader>G',
-        ":terminal git pull && git add . && git commit -m '`date`' && git push<CR> :bd<CR> :echo 'Done!'<CR> ",
-        { buffer = true, desc = 'Update [G]it' }
+        '<leader>w',
+        -- ":terminal git pull && git add . && git commit -m '`date`' && git push<CR>! :bd<CR> :echo 'Done!'<CR> ",
+        ':w<CR>:GitAutoCommitPush<CR>',
+        { buffer = true, desc = '[w]rite (update) with git push', noremap = true }
       )
     end)
   end,
@@ -149,7 +159,7 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'tex' },
   callback = function()
     vim.schedule(function()
-      vim.keymap.set('n', '<leader>w', ':w<CR>:LspTexlabBuild<CR>', { buffer = true, desc = '[w]rite (update) and build' })
+      vim.keymap.set('n', '<leader>w', ':w<CR>:TexlabBuild<CR>', { buffer = true, desc = '[w]rite (update) and build' })
     end)
   end,
 })
@@ -478,7 +488,41 @@ require('lazy').setup({
       -- 'hrsh7th/cmp-nvim-lsp',
       { 'saghen/blink.cmp' },
     },
-    config = function()
+    opts = {
+      servers = {
+        clangd = {},
+        -- gopls = {},
+        ruff = {},
+        -- rust_analyzer = { enabled = false }, -- No need as per rustacean
+        basedpyright = {
+          -- use `uv` to run inside proper environment
+          -- cmd = { 'uv', 'run', 'basedpyright-langserver', '--stdio' },
+          settings = {
+            basedpyright = {
+              analysis = {
+                typeCheckingMode = 'standard',
+              },
+            },
+          },
+        },
+        texlab = {},
+        lua_ls = {
+          -- cmd = { ... },
+          -- filetypes = { ... },
+          -- capabilities = {},
+          settings = {
+            Lua = {
+              completion = {
+                callSnippet = 'Replace',
+              },
+              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+              diagnostics = { disable = { 'missing-fields' } },
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
@@ -575,61 +619,10 @@ require('lazy').setup({
         end
         vim.diagnostic.config { signs = { text = diagnostic_signs } }
       end
-
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      -- local capabilities = require('blink.cmp').get_lsp_capabilities()
       -- local capabilities = vim.lsp.protocol.make_client_capabilities()
       -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        clangd = {},
-        -- gopls = {},
-        ruff = {},
-        -- rust_analyzer = { enabled = false }, -- No need as per rustacean
-        basedpyright = {
-          -- use `uv` to run inside proper environment
-          -- cmd = { 'uv', 'run', 'basedpyright-langserver', '--stdio' },
-          settings = {
-            -- let ruff/isort handle it
-            disableOrganizeImports = true,
-            basedpyright = {
-              analysis = {
-                -- let ruff/mypy handle it
-                ignore = { '*' },
-                typeCheckingMode = 'off',
-              },
-            },
-          },
-        },
-        texlab = {},
-        lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
-      }
+      local servers = {}
       require('mason').setup()
 
       -- You can add other tools here that you want Mason to install
@@ -640,18 +633,26 @@ require('lazy').setup({
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
+      local lspconfig = require 'lspconfig'
+      for server, config in pairs(opts.servers) do
+        -- passing config.capabilities to blink.cmp merges with the capabilities in your
+        -- `opts[server].capabilities, if you've defined it
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+
+      -- require('mason-lspconfig').setup {
+      --   handlers = {
+      --     function(server_name)
+      --       local server = servers[server_name] or {}
+      --       -- This handles overriding only values explicitly passed
+      --       -- by the server configuration above. Useful when disabling
+      --       -- certain features of an LSP (for example, turning off formatting for ts_ls)
+      --       server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+      --       require('lspconfig')[server_name].setup(server)
+      --     end,
+      --   },
+      -- }
     end,
   },
 
@@ -689,260 +690,38 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
   -- lazy.nvim
-  -- {
-  --   'folke/noice.nvim',
-  --   event = 'VeryLazy',
-  --   -- opts = {
-  --   --   -- add any options here
-  --   -- },
-  --   dependencies = {
-  --     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-  --     'MunifTanjim/nui.nvim',
-  --     -- OPTIONAL:
-  --     --   `nvim-notify` is only needed, if you want to use the notification view.
-  --     --   If not available, we use `mini` as the fallback
-  --     -- 'rcarriga/nvim-notify',
-  --   },
-  --
-  --   config = function()
-  --     require('noice').setup {
-  --       lsp = {
-  --         override = {
-  --           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-  --           ['vim.lsp.util.stylize_markdown'] = true,
-  --           ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-  --         },
-  --       },
-  --       -- you can enable a preset for easier configuration
-  --       presets = {
-  --         bottom_search = true, -- use a classic bottom cmdline for search
-  --         -- command_palette = true, -- position the cmdline and popupmenu together
-  --         long_message_to_split = true, -- long messages will be sent to a split
-  --         inc_rename = false, -- enables an input dialog for inc-rename.nvim
-  --         lsp_doc_border = false, -- add a border to hover docs and signature help
-  --       },
-  --       cmdline = { enabled = false },
-  --       -- views = {
-  --       --   mini = {
-  --       --     win_options = { winblend = 0 },
-  --       --   },
-  --       --   cmdline_popup = {
-  --       --     border = {
-  --       --       style = 'single',
-  --       --     },
-  --       --   },
-  --       --   popup = {
-  --       --     border = {
-  --       --       style = 'none',
-  --       --     },
-  --       --   },
-  --       -- },
-  --     }
-  --   end,
-  -- },
-  -- { -- Autocompletion
-  --   'hrsh7th/nvim-cmp',
-  --   event = 'InsertEnter',
-  --   dependencies = {
-  --     -- Snippet Engine & its associated nvim-cmp source
-  --     {
-  --       'L3MON4D3/LuaSnip',
-  --       build = (function()
-  --         -- Build Step is needed for regex support in snippets.
-  --         -- This step is not supported in many windows environments.
-  --         -- Remove the below condition to re-enable on windows.
-  --         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-  --           return
-  --         end
-  --         return 'make install_jsregexp'
-  --       end)(),
-  --       dependencies = {
-  --         -- `friendly-snippets` contains a variety of premade snippets.
-  --         --    See the README about individual language/framework/plugin snippets:
-  --         --    https://github.com/rafamadriz/friendly-snippets
-  --         {
-  --           'rafamadriz/friendly-snippets',
-  --           config = function()
-  --             require('luasnip.loaders.from_vscode').lazy_load()
-  --           end,
-  --         },
-  --         {
-  --           'gitaarik/nvim-cmp-toggle',
-  --           config = function()
-  --             vim.api.nvim_set_keymap('n', '<Leader>tc', ':NvimCmpToggle<CR>', { noremap = true, silent = true, desc = 'Toggle [c]ompletions' })
-  --           end,
-  --         },
-  --       },
-  --     },
-  --     'saadparwaiz1/cmp_luasnip',
-  --
-  --     -- Adds other completion capabilities.
-  --     --  nvim-cmp does not ship with all sources by default. They are split
-  --     --  into multiple repos for maintenance purposes.
-  --     'hrsh7th/cmp-nvim-lsp',
-  --     'hrsh7th/cmp-path',
-  --     'onsails/lspkind.nvim',
-  --     'hrsh7th/cmp-cmdline',
-  --     'hrsh7th/cmp-buffer',
-  --   },
-  --   config = function()
-  --     -- See `:help cmp`
-  --     local cmp = require 'cmp'
-  --     local luasnip = require 'luasnip'
-  --     local lspkind = require 'lspkind'
-  --     luasnip.config.setup {}
-  --
-  --     cmp.setup {
-  --       snippet = {
-  --         expand = function(args)
-  --           luasnip.lsp_expand(args.body)
-  --         end,
-  --       },
-  --       completion = { completeopt = 'menu,menuone,noinsert' },
-  --       formatting = {
-  --         format = lspkind.cmp_format(),
-  --       },
-  --
-  --       -- For an understanding of why these mappings were
-  --       -- chosen, you will need to read `:help ins-completion`
-  --       --
-  --       -- No, but seriously. Please read `:help ins-completion`, it is really good!
-  --       mapping = cmp.mapping.preset.insert {
-  --         -- Select the [n]ext item
-  --         ['<C-n>'] = cmp.mapping.select_next_item(),
-  --         -- Select the [p]revious item
-  --         ['<C-p>'] = cmp.mapping.select_prev_item(),
-  --
-  --         -- Scroll the documentation window [b]ack / [f]orward
-  --         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  --         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  --
-  --         -- Accept ([y]es) the completion.
-  --         --  This will auto-import if your LSP supports it.
-  --         --  This will expand snippets if the LSP sent a snippet.
-  --         ['<C-y>'] = cmp.mapping.confirm { select = true },
-  --
-  --         -- If you prefer more traditional completion keymaps,
-  --         -- you can uncomment the following lines
-  --         --['<CR>'] = cmp.mapping.confirm { select = true },
-  --         --['<Tab>'] = cmp.mapping.select_next_item(),
-  --         --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-  --
-  --         -- Manually trigger a completion from nvim-cmp.
-  --         --  Generally you don't need this, because nvim-cmp will display
-  --         --  completions whenever it has completion options available.
-  --         ['<C-Space>'] = cmp.mapping.complete {},
-  --
-  --         -- Think of <c-l> as moving to the right of your snippet expansion.
-  --         --  So if you have a snippet that's like:
-  --         --  function $name($args)
-  --         --    $body
-  --         --  end
-  --         --
-  --         -- <c-l> will move you to the right of each of the expansion locations.
-  --         -- <c-h> is similar, except moving you backwards.
-  --         ['<C-l>'] = cmp.mapping(function()
-  --           if luasnip.expand_or_locally_jumpable() then
-  --             luasnip.expand_or_jump()
-  --           end
-  --         end, { 'i', 's' }),
-  --         ['<C-h>'] = cmp.mapping(function()
-  --           if luasnip.locally_jumpable(-1) then
-  --             luasnip.jump(-1)
-  --           end
-  --         end, { 'i', 's' }),
-  --
-  --         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-  --         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-  --       },
-  --       sources = {
-  --         {
-  --           name = 'lazydev',
-  --           -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-  --           group_index = 0,
-  --         },
-  --         { name = 'nvim_lsp' },
-  --         { name = 'luasnip' },
-  --         { name = 'path' },
-  --         { name = 'cmp-cmdline' },
-  --         { name = 'cmp-buffer' },
-  --       },
-  --       -- view = {
-  --       --   entries = { name = 'custom', selection_order = 'near_cursor' },
-  --       -- },
-  --     }
-  --
-  --     -- `/` cmdline setup.
-  --     cmp.setup.cmdline('/', {
-  --       mapping = cmp.mapping.preset.cmdline(),
-  --       sources = {
-  --         { name = 'buffer' },
-  --       },
-  --     })
-  --     cmp.setup.cmdline(':', {
-  --       mapping = cmp.mapping.preset.cmdline(),
-  --       sources = cmp.config.sources({
-  --         { name = 'path' },
-  --       }, {
-  --         {
-  --           name = 'cmdline',
-  --           option = {
-  --             ignore_cmds = { 'Man', '!' },
-  --           },
-  --         },
-  --       }),
-  --     })
-  --   end,
-  -- },
   { 'rktjmp/shipwright.nvim' },
-  -- {
-  --   'ramojus/mllifluous.nvim',
-  --   -- version = "v0.*", -- uncomment for stable config (some features might be missed if/when v1 comes out)
-  --   config = function()
-  --     require('mellifluous').setup {
-  --       highlight_overrides = {
-  --         dark = function(highlighter, colors) -- dark theme
-  --           -- set highlights here (using highlighter)
-  --           highlighter.set()
-  --         end,
-  --         light = function(highlighter, colors) -- light theme
-  --           -- set highlights here (using highlighter)
-  --         end,
-  --       },
-  --     } -- optional, see configuration section.
-  --     vim.cmd 'colorscheme mellifluous'
-  --   end,
-  -- },
-  -- {
-  --   'bluz71/vim-moonfly-colors',
-  --   name = 'moonfly',
-  --   lazy = false,
-  --   priority = 1000,
-  --   init = function()
-  --     vim.opt.termguicolors = true
-  --     vim.g.moonflyItalics = false
-  --     vim.cmd.colorscheme 'moonfly'
-  --   end,
-  -- },
   {
-    'savq/melange-nvim',
-
-    dependencies = 'rktjmp/lush.nvim',
+    'wtfox/jellybeans.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      -- plugins = { auto = false },
+      italics = false,
+      on_colors = function(c)
+        local light_bg = '#f9f9f9' -- Change the default light bg
+        local dark_bg = '#151515'
+        c.background = vim.o.background == 'light' and light_bg or dark_bg
+      end,
+    },
     init = function()
-      -- vim.g.melange_enable_font_variants = 0 -- disable font variants
-      vim.opt.termguicolors = true
-      vim.cmd.colorscheme 'melange'
+      vim.cmd [[colorscheme jellybeans ]]
     end,
   },
+  -- {
+  --   'savq/melange-nvim',
+  --
+  --   dependencies = 'rktjmp/lush.nvim',
+  --   init = function()
+  --     -- vim.g.melange_enable_font_variants = 0 -- disable font variants
+  --     vim.opt.termguicolors = true
+  --     vim.cmd.colorscheme 'melange'
+  --   end,
+  -- },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -1040,28 +819,19 @@ require('lazy').setup({
         enabled = true,
         -- Maybe later
         preset = {
-          -- header = [[
-          --
-          -- ####
-          --  ####
-          --    ####
-          --      ####
-          --        #####
-          --        #######
-          --        #### ####
-          --        ####   ####
-          --        ####     ####
-          --       ####       ####
-          --          ]],
           header = [[
- ________  ___       ___               ________  ________  ________  ________          
-|\   __  \|\  \     |\  \             |\   ____\|\   __  \|\   __  \|\   ____\         
-\ \  \|\  \ \  \    \ \  \            \ \  \___|\ \  \|\  \ \  \|\  \ \  \___|_        
- \ \   __  \ \  \    \ \  \            \ \  \    \ \   __  \ \   ____\ \_____  \       
-  \ \  \ \  \ \  \____\ \  \____        \ \  \____\ \  \ \  \ \  \___|\|____|\  \  ___ 
-   \ \__\ \__\ \_______\ \_______\       \ \_______\ \__\ \__\ \__\     ____\_\  \|\__\
-    \|__|\|__|\|_______|\|_______|        \|_______|\|__|\|__|\|__|    |\_________\|__|
- ]],
+
+####
+  ####
+    ####
+       ####
+       #####
+       #######
+       #### ####
+       ####   ####
+       ####     ####
+       ####       ####
+                             ]],
         -- stylua: ignore
         ---@type snacks.dashboard.Item[]
         keys = {
